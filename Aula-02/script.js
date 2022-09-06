67cf6157605ac812b4e655e4aaf42f18ac9c6752
Brasilino de Souza Neto <102764942+brasilinodesouza@users.noreply.github.com>
2022-09-06 11:57:04 -0300

    aula02

--- Aula-02/script.js
+++ Aula-02/script.js
@@ -1,13 +1,19 @@
 
-function Converter(){
+function ConverterDolar(){
     var valorElemento = document.getElementById("valor");
     var valor = valorElemento.value;
     var valorEmDolarNumerico = parseFloat(valor);
-    var valorEmReal = valorEmDolarNumerico * 5 ;
-
-    var elementoValorConvertido = document.getElementById("valorConvertido");
-
-    var valorConvertido = "O valor convertido é R$ " + valorEmReal;
+    var valorEmDolar = valorEmDolarNumerico / 5 ;
+    var elementoValorConvertido = document.getElementById("valorConvertidoEmDolar");
+    var valorConvertido = "O valor convertido em dolar é $" +valorEmDolar;
     elementoValorConvertido.innerHTML = valorConvertido;
-
+}
+function ConverterEuro(){
+    var valorElemento = document.getElementById("valor");
+    var valor = valorElemento.value;
+    var valorEmDolarNumerico = parseFloat(valor);
+    var valorEmEuro = valorEmDolarNumerico / 5.13 ;
+    var elementoValorConvertido = document.getElementById("valorConvertidoEmEuro");
+    var valorConvertido = "O valor convertido em euro €" +valorEmEuro;
+    elementoValorConvertido.innerHTML = valorConvertido;
 }
