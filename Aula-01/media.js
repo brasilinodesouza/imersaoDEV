a6a5b358d4141c04695f76531b264e39edbbcc67
Brasilino de Souza Neto <102764942+brasilinodesouza@users.noreply.github.com>
2022-09-06 01:27:57 -0300

    .

---
+++ Aula-01/media.js
@@ -0,0 +1,10 @@
+var nome = "Neto";
+var notaPrimeiroBimestre = 9;
+var notaSegundoBimestre = 7;
+var notaTerceiroBimestre = 5;
+var notaQuartoBimestre = 6;
+
+var notaFinal = (notaPrimeiroBimestre + notaSegundoBimestre + notaTerceiroBimestre + notaQuartoBimestre) / 4; 
+
+console.log("Bem vindo " +nome);
+console.log(notaFinal);
