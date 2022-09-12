6a15584a4a82ec049a147efa25b86dd78f1847b3
Brasilino de Souza Neto <102764942+brasilinodesouza@users.noreply.github.com>
2022-09-12 09:46:31 -0300

    aula-04
    aula-04

---
+++ Aula-04/script.js
@@ -0,0 +1,15 @@
+var listaFilme = [
+"https://upload.wikimedia.org/wikipedia/pt/thumb/9/92/Karate_kid_P%C3%B4ster.jpg/200px-Karate_kid_P%C3%B4ster.jpg",
+
+"https://br.web.img3.acsta.net/medias/nmedia/18/92/61/40/20208886.jpg",
+
+"https://br.web.img2.acsta.net/medias/nmedia/18/92/61/42/20208888.jpg", 
+
+"https://br.web.img3.acsta.net/pictures/22/08/16/18/43/4433326.jpg"];
+
+
+
+for (var i = 0; i < listaFilme.length; i++ ){
+
+    document.write("<img src =" +listaFilme[i]+">");
+}
